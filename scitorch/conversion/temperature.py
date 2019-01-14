252b45e94cf117658e97688c8bbcb02ee17c7dc4
christoph.fritz <christoph.fritz@fabasoft.com>
2019-01-14 15:32:47 +0100

    revert changes and convert in file instead of moving it to a helper file

--- scitorch/conversion/temperature.py
+++ scitorch/conversion/temperature.py
@@ -1,6 +1,5 @@
 """Conversion of different temperature scales (Kelvin, Fahrenheit and Celsius)."""
 
-from scitorch.conversion._standard_units import kelvin
 from scitorch.tools._tensors import _create_tensor
 
 def to_kelvin(val=0.0, scale='k'):
@@ -25,7 +24,17 @@ def to_kelvin(val=0.0, scale='k'):
     tensor(273.1500, dtype=torch.float64)
 
     """
-    return kelvin(val, scale)
+    temp = _create_tensor(val)
+    if scale == 'k':
+        temp = temp
+    elif scale == 'c':
+        temp = temp + 273.15
+    elif scale == 'f':
+        temp = (temp - 32) * 5/9 + 273.15
+    else:
+        raise NotImplementedError(f'{scale} is not supported. See documentation for available scales.')
+
+    return temp
 
 def to_celsius(val=0.0, scale='c'):
     """Converts a value from Kelvin/Fahrenheit to Celsius.
@@ -53,7 +62,7 @@ def to_celsius(val=0.0, scale='c'):
     if scale =='c':
         temp = _create_tensor(val)
     else:
-        temp = kelvin(val, scale)
+        temp = to_kelvin(val, scale)
         temp = temp - 273.15
 
     return temp
@@ -84,7 +93,7 @@ def to_fahrenheit(val=0.0, scale='f'):
     if scale == 'f':
         temp = _create_tensor(val)
     else:
-        temp = kelvin(val, scale)
+        temp = to_kelvin(val, scale)
         temp = (temp - 273.15) * 9/5 + 32
 
     return temp
